96c54580bb599a9547c6d7a92eb8b020da72661f
Nadav Ivgi <nadav@shesek.info>
2017-07-19 20:22:58 +0300

    simplify, no callback handling

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { Observable as Obs } from 'rxjs'
 import vex from 'vex-js'
 
-module.exports = (defaultOpt={}) => open$ => {
-  let _vex = Obs.bindCallback((id, method, options, cb) =>
-    vex.dialog[method](Object.assign({}, defaultOpt, options, { callback: value => cb({ id, value }) })))
-  return Obs.from(open$).flatMap(opt => _vex(opt.id, opt.method || 'open', opt))
-}
+vex.registerPlugin(require('vex-dialog'))
+
+module.exports = (defaultOpt={}) => open$ =>
+  Obs.from(open$).subscribe(opt => vex.dialog[opt.method](Object.assign({}, defaultOpt, opt)))
