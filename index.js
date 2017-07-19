2ed7441e6af0cbe9e99d29dcc054da374771804d
Nadav Ivgi <nadav@shesek.info>
2017-07-19 22:15:39 +0300

    close previous dialogs before opening a new one

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ import vex from 'vex-js'
 vex.registerPlugin(require('vex-dialog'))
 
 module.exports = (defaultOpt={}) => open$ =>
-  Obs.from(open$).subscribe(opt => vex.dialog[opt.method](Object.assign({}, defaultOpt, opt)))
+  Obs.from(open$).subscribe(opt => (vex.closeAll(), vex.dialog[opt.method](Object.assign({}, defaultOpt, opt))))
